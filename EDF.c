8e627194da2ee71479ab7437dc9e2b1a45394aaf
lobao5670 <lobao5670@gmail.com>
2020-09-16 22:23:52 -0300

    Escalonador EDF em C

---
+++ EDF.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+
+int QTD = 3;
+
+int main()
+{
+    int processos[QTD][4];
+    
+    processos[0][0] = 0;
+    processos[0][1] = 3;
+    processos[0][2] = 7;
+    processos[0][3] = 20;
+    
+    processos[1][0] = 1;
+    processos[1][1] = 2;
+    processos[1][2] = 4;
+    processos[1][3] = 5;
+    
+    processos[2][0] = 2;
+    processos[2][1] = 2;
+    processos[2][2] = 8;
+    processos[2][3] = 10;
+    
+    edf(processos);
+
+    return 0;
+}
+
+int hiper_periodo(int processos[QTD][4]) {
+    int temp = 0;
+    
+    for (int i = 0; i < QTD; i++) {
+        if(processos[i][3] > 3) {
+            temp = processos[i][3];
+        }
+    }
+    
+    return temp;
+}
+
+int escolher_menor_deadline(int processos[QTD][4], int deadlines[QTD]) {
+    int menor_deadline = 10000;
+    int escolhido = -1;
+
+    for (int i = 0; i < QTD; i++) {
+        if (deadlines[i] < menor_deadline) {
+            menor_deadline = deadlines[i];
+            escolhido = i;
+        }
+    }
+
+    return escolhido;
+}
+
+void edf(int processos[QTD][4]) {
+    int relogio = 0;
+
+    int deadlines[QTD];
+    for (int i = 0; i < QTD; i++) {
+        deadlines[i] = processos[i][2];
+    }
+
+    int periodos[QTD];
+    for (int i = 0; i< QTD; i++) {
+        periodos[i] = processos[i][3];
+    }
+
+    int contador[QTD];
+
+    for (;;) {
+        int escolhido = escolher_menor_deadline(processos, deadlines);
+        printf("Processo escolhido: %d\n", escolhido);
+
+        if (periodos[escolhido] >= relogio) {
+            relogio += processos[escolhido][1];
+            printf("Processo: P%d executando...\n", escolhido);
+            printf("Relogio: %d\n", relogio);
+            printf("Burst time do processo P%d: %d\n", escolhido, processos[escolhido][1]);
+
+            printf("Deadline ANTERIOR do processo: %d\n", deadlines[escolhido]);
+            deadlines[escolhido] += processos[escolhido][3];
+            printf("Deadline do processo P%d atualizado: %d\n", escolhido, deadlines[escolhido]);
+
+            printf("Periodo ANTERIOR do processo: %d\n", periodos[escolhido]);
+            periodos[escolhido] += processos[escolhido][3];
+            printf("Periodo do processo P%d atualizo: %d\n", escolhido, periodos[escolhido]);
+
+            contador[escolhido]++;
+        }
+
+        if (relogio >= 20) {
+            break;
+        }
+    }
+
+    for (int i = 0; i < QTD; i++) {
+        printf("O processo P%d executou %d vezes\n", i, contador[i]);
+    }
+}
